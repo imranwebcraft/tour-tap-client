2a47fd4136782f6cd30089ef12b63fe91f3f1441
Imran Hossain Sakil <112363382+imranwebcraft@users.noreply.github.com>
2023-11-27 12:43:08 +0600

    Wishlist handler and send wishlist data to the database

--- src/Pages/Home/components/TravelGuide/OurPackage/PackageCard/PackageCard.jsx
+++ src/Pages/Home/components/TravelGuide/OurPackage/PackageCard/PackageCard.jsx
@@ -2,14 +2,53 @@ import { useState } from 'react';
 import MotionButton from '../../../../../../Components/MotionButton/MotionButton';
 import { FaRegHeart } from 'react-icons/fa6';
 import { Link } from 'react-router-dom';
+import { Tooltip } from 'react-tooltip';
+import useAuth from '../../../../../../Hook/useAuth';
+import useAxiosPublic from '../../../../../../Hook/useAxiosPublic';
+import { toast } from 'react-hot-toast';
+
 const PackageCard = ({ packageItem }) => {
+	const axiosPublic = useAxiosPublic();
 	const [wishlist, setWishlist] = useState(false);
+	const { user } = useAuth();
 
 	// Sate to manage show all button
-	const { _id, image, tourType, tripTitle, price } = packageItem || {};
+	const { _id, image, tourType, tripTitle, price, tourDetails } =
+		packageItem || {};
+
+	const handleWishlist = async () => {
+		if (!wishlist) {
+			console.log('Wishlist true');
+			const wishlistData = {
+				email: user?.email,
+				image: image,
+				tourType: tourType,
+				tripTitle: tripTitle,
+				price: price,
+				tourDetails: tourDetails,
+			};
+			await axiosPublic
+				.post('/save-wishlist', wishlistData)
+				.then(() => {
+					toast.success('Added to wishlist');
+					setWishlist(true);
+				})
+				.catch((err) => {
+					console.log(err.message);
+				});
+
+			setWishlist(true);
+		} else {
+			toast.error('Already added to wishlist');
+		}
+	};
 
 	return (
 		<section className="group tracking-wide">
+			<Tooltip
+				id="my-tooltip"
+				style={{ backgroundColor: 'rgba(244, 63, 94, 0.9)', color: 'white' }}
+			/>
 			<div className="mx-auto flex flex-col h-full overflow-hidden  justify-center bg-white rounded-2xl shadow-md shadow-gray-400/20 group-hover:shadow-lg transition-all duration-300">
 				<div className=" relative mb-2">
 					<img
@@ -17,9 +56,13 @@ const PackageCard = ({ packageItem }) => {
 						src={image}
 						alt={tripTitle}
 					/>
-					<span className=" absolute right-2 top-2 hover:cursor-pointer hover:scale-110 transition-all duration-300 hover:shadow-md">
+					<span
+						onClick={handleWishlist}
+						data-tooltip-id="my-tooltip"
+						data-tooltip-content="Add To Wishlist"
+						className=" absolute right-2 top-2 hover:cursor-pointer hover:scale-110 transition-all duration-300 hover:shadow-md"
+					>
 						<FaRegHeart
-							onClick={() => setWishlist(!wishlist)}
 							className={`text-3xl  rounded-md  px-2 py-0.5
 							${wishlist ? 'bg-rose-500 text-white' : 'text-rose-500 bg-white'}
 							transition-all duration-300
